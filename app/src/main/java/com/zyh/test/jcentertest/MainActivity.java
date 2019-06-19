9c01b2db743b1daaebf3d67b403ae677521c9b00
zhangyonghui <zhangyonghui@aerozhonghuan.com>
2019-06-19 16:39:25 +0800

    配置bintray

--- app/src/main/java/com/zyh/test/jcentertest/MainActivity.java
+++ app/src/main/java/com/zyh/test/jcentertest/MainActivity.java
@@ -3,11 +3,14 @@ package com.zyh.test.jcentertest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.zyh.test.simplevideoplayer.VideoRecordManager;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        VideoRecordManager.getInstance().startRecord("", this);
     }
 }
